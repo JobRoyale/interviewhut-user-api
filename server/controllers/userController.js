d6d0b2ed61f04619bda29e25380942cf4979eb81
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2021-04-07 22:34:11 +0530

    Added precheck and fixed logout

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,9 +5,16 @@ const {
   getCookieOptions,
   getUserNameToken,
   getAccessToken,
+  verifyToken,
 } = require('../utils/auth');
 const googleAuth = require('../utils/googleAuth');
 const SERVER_RESPONSE = require('../utils/serverResponses');
+
+// Destructing array containing secrets for refresh and access token
+const [ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET] = [
+  process.env.ACCESS_TOKEN_SECRET,
+  process.env.REFRESH_TOKEN_SECRET,
+];
 
 const signUpUser = (req, res) => {
   try {
@@ -174,6 +181,91 @@ const loginUser = (req, res) => {
   }
 };
 
+const preCheckUser = async (req, res) => {
+  try {
+    // Extracting accessToken from headers passed by client
+    let accessToken = req.headers.authorization.split(' ')[1];
+    // Extracting userNameToken, refreshToken from cookies
+    const refreshToken = req.cookies.interviewhut_rtk;
+    const userNameToken = req.cookies.interviewhut_u;
+
+    let payload;
+
+    if (!userNameToken) {
+      res.status(403).json({
+        status: false,
+        payload: {
+          message: SERVER_RESPONSE.LOGINREQUIRED,
+        },
+      });
+    }
+
+    let username;
+
+    // Extracting username from userNameToken
+    try {
+      username = verifyToken(userNameToken, ACCESS_TOKEN_SECRET).username;
+    } catch (error) {
+      console.log(error);
+      throw new Error('Token Not Provided');
+    }
+
+    // Verifying accessToken
+    try {
+      payload = verifyToken(accessToken, ACCESS_TOKEN_SECRET + username);
+    } catch (error) {
+      if (error.message !== 'jwt expired') {
+        throw new Error('Token Man Handled');
+      }
+    }
+
+    // If accessToken verification failed, thats means server has to renew accessToken
+    if (!payload) {
+      // Getting userData from db
+      const user = await User.findOne({ username });
+
+      // Verifying refreshToken
+      payload = verifyToken(refreshToken, REFRESH_TOKEN_SECRET + user.password);
+
+      // Both accessToken and refreshToken are invalid
+      if (!payload) {
+        throw new Error('Auth Failed');
+      }
+
+      // Renewing tokens
+      accessToken = getAccessToken(user);
+      res.cookie(
+        'interviewhut_rtk',
+        getRefreshToken(user),
+        getCookieOptions(604800000)
+      );
+      res.cookie(
+        'interviewhut_u',
+        getUserNameToken(user),
+        getCookieOptions(604800000)
+      );
+    }
+
+    res.status(200).json({
+      status: true,
+      payload: {
+        message: SERVER_RESPONSE.TOKEN,
+        accessToken: accessToken,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.clearCookie('grupo_rtk');
+    res.clearCookie('grupo_u');
+    res.status(401).json({
+      status: false,
+      payload: {
+        message: SERVER_RESPONSE.AUTHERROR,
+      },
+    });
+  }
+};
+
 const logoutUser = (req, res) => {
   try {
     res.clearCookie('interviewhut_rtk');
@@ -185,7 +277,6 @@ const logoutUser = (req, res) => {
       },
     });
   } catch (error) {
-    console.log(error);
     res.status(500).json({
       status: false,
       payload: {
@@ -195,4 +286,4 @@ const logoutUser = (req, res) => {
   }
 };
 
-module.exports = { signUpUser, loginUser, logoutUser };
+module.exports = { signUpUser, loginUser, preCheckUser, logoutUser };
--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,7 @@ const { auth } = require('google-auth-library');
 const {
   signUpUser,
   loginUser,
+  preCheckUser,
   logoutUser,
 } = require('../controllers/userController');
 const authRequest = require('../middlewares/authRequest');
@@ -11,6 +12,7 @@ const usersRouter = express.Router();
 
 usersRouter.post('/signup', signUpUser);
 usersRouter.post('/login', loginUser);
-usersRouter.get('/logout', authRequest, logoutUser);
+usersRouter.get('/precheck', preCheckUser);
+usersRouter.get('/logout', logoutUser);
 
 module.exports = usersRouter;
